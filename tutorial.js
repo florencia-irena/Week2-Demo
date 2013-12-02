0921de85d062332754f8beabb840f9ba7026ffda
aolrich23 <andrea.olrich@gmail.com>
2013-12-02 16:54:50 +1100

    creating html elements

--- tutorial.js
+++ tutorial.js
@@ -1,1 +1,14 @@
 //Javascript file for tutorial
+//THE GLOBAL VARIABLES
+
+//object to store lesson information
+function Lesson(title, blurb, divID) {
+  this.title = title;
+  this.blurb = blurb;
+  this.divID = divID;
+  this.update = function() {};
+}
+
+//TODO: Create Lesson Variables for all seven lessons
+
+function clear();
