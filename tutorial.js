150f9c9693c04599e08f02fbfa4a8fcac347c441
florencia-irena <florencia.irena94@gmail.com>
2013-12-03 09:33:40 +1100

    first sync

--- tutorial.js
+++ tutorial.js
@@ -18,6 +18,67 @@ var lessonArray = [lesson0, lesson1, lesson2];
 //Finding objects with class "lesson"
 var lessonsClass = document.getElementsByClassName("lesson");
 
+//*****************THE GLOBAL FUNCTIONS**********************//
+google.maps.event.addDomListener(window, 'load', function initialize(){
+  //CREATING BUTTONS
+  for (var i=0; i<lessonsClass.length; i++){
+    makeButton(lessonsClass[i].id, i);
+  }
+  
+  //create refresh button
+  var button = document.getElementById("buttons");
+  var newButton = document.createElement("input");
+  newButton.type = "button";
+  newButton.id = "refresh";
+  newButton.value = "Refresh";
+  newButton.onclick = function(){
+    refresh();
+  };
+  button.appendChild(newButton);
+  
+  //button.innerHTML = button.innerHTML + "<input type=\"button\" id=\"refresh\" value=\"Refresh\" onclick=\"refresh()\"></input><br>";
+  var buttonElement = document.getElementById("refresh");
+  buttonStyle (buttonElement, lessonsClass.length);
+
+  //Set the initial page to be introduction
+  lessonArray[0].refresh();
+});
+
+function makeButton(string, i){
+  var button = document.getElementById("buttons");
+  var newButton = document.createElement("input");
+  newButton.type = "button";
+  newButton.id = string+"button";
+  newButton.value = lessonArray[i].title;
+  newButton.onclick = function(){
+    lessonArray[i].update()
+  };
+  button.appendChild(newButton);
+  button.appendChild(document.createElement("br"));
+  var buttonElement = document.getElementById(string+"button");
+  buttonStyle(buttonElement, i);
+}
+
+function buttonStyle(buttonProp, i){
+  buttonProp.style.display = ' ';
+  buttonProp.style.backgroundColor = 'yellow';
+  buttonProp.style.width = '150px';
+  buttonProp.style.height = '40px';
+  buttonProp.style.fontSize = '20px';
+  buttonProp.style.opacity = 0.8;
+  buttonProp.style.fontWeight = 'bold';
+  buttonProp.style.color = 'black';
+  buttonProp.onmouseover = function(){  
+    buttonProp.style.backgroundColor = color[i%color.length];
+    buttonProp.style.color = 'white';
+  }
+  buttonProp.onmouseout = function(){
+    buttonProp.style.backgroundColor = 'yellow';
+    buttonProp.style.color = 'black';
+  }
+}
+
+
 function clear(){
   for (var i=0; i<lessonArray.length; i++){
     if(lessonArray[i].title === document.title){
