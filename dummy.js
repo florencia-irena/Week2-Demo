968bfd7011306c1ba5e4788adf8e5d37ae1c8f7d
aolrich23 <andrea.olrich@gmail.com>
2013-12-06 14:39:38 +1100

    added collapsible menu

--- dummy.js
+++ dummy.js
@@ -1,13 +1,36 @@
   $(document).ready( function() {
-      resizeText()
+    prepareMenu()
+    /*
+    $(window).resize(function() {
+      $urlHeight = $('#url').height();
+      $urlWidth = $('#url').width();
+      if ($urlHeight*0.8 < $urlWidth*0.9) {
+        $('#url').css('font-size', $urlHeight*0.8)
+      } else {
+        $('#url').css('font-size', $urlWidth*0.9)
+      }
+    });
+  */
   });
 
-  function resizeText() {
-    $urlHeight = $('#url').height();
-    if($urlHeight < 30) {
-      $('#url').css('font-size', 25)
-    }
-    if ($urlHeight >= 35) {
-      $('#url').css('font-size', 30)
-    }
-  }
+  function prepareMenu() {
+    $('#expList').find('li:has(ul)')
+      .click( function(event) {
+        if (this == event.target) {
+          $(this).toggleClass('expanded');
+          $(this).children('ul').toggle('medium');
+        }
+        return false;
+      })
+      .addClass('collapsed')
+      .children('ul').hide();
+
+      $list = $('#list0');
+      $('#lesson0').click( function(event) {
+          if (this == event.target) {
+            $list.toggleClass('expanded');
+            $list.children('ul').toggle('medium');
+          }
+          return false;
+      })
+  };
