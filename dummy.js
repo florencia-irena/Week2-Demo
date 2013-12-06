cffdfea460fd3c358689f4203824dc6d8a9808ea
florencia-irena <florencia.irena94@gmail.com>
2013-12-06 14:35:55 +1100

    font resizing

---
+++ dummy.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  //GME API TITLE
+  $("#title").css('font-size', 0.031*($("#title").height()+$("#title").width()));
+  //INSTRUCTIONS
+  $("#instructions").css('font-size', 0.021*($("#instructions").height()+$("#instructions").width()));
+  //URL BOX
+  $("#url").css('font-size', 0.26*($("#url").height()+$("#url").width()));
+  //URL INPUT
+  $("#input-url").css('font-size', 0.036*($("#input-url").height()+0.5*$("#input-url").width()));
+  //JSON INPUT
+  $("#input-js").css('font-size', 0.013*($("#input-js").height()+$("#input-js").width()));
+  //OUTPUT
+  $("#output").css('font-size', 0.013*($("#output").height()+$("#output").width()));
+  //LESSON BUTTONS
+  $(".button").css('font-size', 0.060*($(".button").height()+$(".button").width()));
+  $(".button-child").css('font-size', 0.045*($(".button-child").height()+$(".button-child").width()));
+  //SUBMIT AND CLEAR BUTTONS
+  $("#submit-button").css('font-size', 0.18*($("#submit-button").height()+$("#submit-button").width()));
+  $("#clear-button").css('font-size', 0.18*($("#clear-button").height()+$("#submit-button").width()));
+});
+
+$(window).resize(function() {
+  //GME API TITLE
+  $("#title").css('font-size', 0.031*($("#title").height()+$("#title").width()));
+  //INSTRUCTIONS
+  $("#instructions").css('font-size', 0.021*($("#instructions").height()+$("#instructions").width()));
+  //URL BOX
+  $("#url").css('font-size', 0.26*($("#url").height()+$("#url").width()));
+  //URL INPUT
+  $("#input-url").css('font-size', 0.036*($("#input-url").height()+0.5*$("#input-url").width()));
+  //JSON INPUT
+  $("#input-js").css('font-size', 0.013*($("#input-js").height()+$("#input-js").width()));
+  //OUTPUT
+  $("#output").css('font-size', 0.013*($("#output").height()+$("#output").width()));
+  //LESSON BUTTONS
+  $(".button").css('font-size', 0.060*($(".button").height()+$(".button").width()));
+  $(".button-child").css('font-size', 0.045*($(".button-child").height()+$(".button-child").width()));
+  //SUBMIT AND CLEAR BUTTONS
+  $("#submit-button").css('font-size', 0.18*($("#submit-button").height()+$("#submit-button").width()));
+  $("#clear-button").css('font-size', 0.18*($("#clear-button").height()+$("#submit-button").width()));
+});
